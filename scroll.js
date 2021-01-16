36b0320f75f70b76548dcc14e29180a27d52adba
Rosanne Kruithof <rosannekruithof@gmail.com>
2021-01-16 18:39:08 +0100

    water animation

--- scroll.js
+++ scroll.js
@@ -13,6 +13,29 @@ const tl = gsap.timeline({
   },
 });
 
+// gsap.set("#wave-right", { xPercent: 97 });
+// gsap.set("#waves", { scaleX: 2, transformOrigin: "0 50%" });
+
+// Wave rotation
+
+// const waveDarkRotation = gsap.to("#wave-dark-mask .wave-rotate", {
+//   rotate: 360,
+//   transformOrigin: "50% 50%",
+//   duration: 6,
+//   ease: "linear",
+//   repeat: -1,
+// });
+
+// const waveLightRotation = gsap.from("#wave-light-mask .wave-rotate", {
+//   rotate: 360,
+//   transformOrigin: "50% 50%",
+//   duration: 6,
+//   ease: "linear",
+//   repeat: -1,
+// });
+
+// Timeline
+
 tl.to("#tablets-svg .text", { y: 20, opacity: 0 })
   .add("tabletMorph")
   .to(".tablet.bathroom", {
@@ -56,14 +79,42 @@ tl.to("#tablets-svg .text", { y: 20, opacity: 0 })
     y: -20,
     opacity: 0,
   })
-  .from("#waves", { yPercent: 100 })
   .from("#step2", {
     y: 50,
     opacity: 0,
   })
+  .to(
+    "#wave-dark-mask .wave-rotate",
+    {
+      rotate: 360,
+      transformOrigin: "50% 50%",
+      duration: 2,
+      ease: "linear",
+      repeat: 1,
+    },
+    "<"
+  )
+  .from(
+    "#wave-light-mask .wave-rotate",
+    {
+      rotate: 360,
+      transformOrigin: "50% 50%",
+      duration: 2,
+      ease: "linear",
+      repeat: 1,
+    },
+    "<"
+  )
+  .from("#waves", { yPercent: 100, duration: 3 }, "<")
   .from("#tip2", {
     opacity: 0,
   })
+  //   .add(waveDarkRotation.pause())
+  //   .add(waveLightRotation.pause())
+  //   .to(waveDarkRotation, { paused: true })
+  //   .to(waveLightRotation, { paused: true })
+  //   .call(() => waveDarkRotation.pause())
+  //   .call(() => waveLightRotation.pause())
   .to("#step2, #tip2", {
     y: -20,
     opacity: 0,
@@ -73,9 +124,10 @@ tl.to("#tablets-svg .text", { y: 20, opacity: 0 })
     opacity: 0,
   })
   .add("cap")
-  .from("#bottle-cap", { yPercent: -100, opacity: 0 })
+  .from("#bottle-cap", { yPercent: -100, opacity: 0 }, "<")
   .to("#bottle-cap", { x: -8 }, "<")
   .fromTo("#bottle-cap", { x: -8 }, { x: 0, duration: 0.5 }, "-=0.5")
+  .from("#waves", { opacity: 0.5 }, "<")
   .from("#tip3", {
     opacity: 0,
   })
