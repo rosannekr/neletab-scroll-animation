70ddc2348757358e6d9158574a5134e2ac241655
Rosanne Kruithof <rosannekruithof@gmail.com>
2021-01-21 11:15:57 +0100

    layout changes

--- animation.js
+++ animation.js
@@ -41,13 +41,9 @@ tl.to("#tablets-svg .text", { y: 10, opacity: 0 })
   .to(".tablet.multi, .tablet.bathroom", { opacity: 0 }, "-=0.8")
   .to(".tablet.window", { opacity: 0 })
   .from("#tablet-img", { opacity: 0 }, "<")
-  .to(
-    "#tablets-svg",
-    {
-      attr: { viewBox: "0 -25 792.82 180" },
-    },
-    "<"
-  )
+  .to("#tablets-svg", {
+    attr: { viewBox: "0 0 792.82 130" },
+  })
   .from("#tablet-title", { opacity: 0, y: 20, ease: "power1" })
   .from("#tablet-text", { opacity: 0, y: 20, ease: "power1" })
   .to("#tablet-text span:nth-of-type(1)", { opacity: 0, y: -15 }, "+=0.5")
@@ -58,87 +54,114 @@ tl.to("#tablets-svg .text", { y: 10, opacity: 0 })
   .from("#tablet-text span:nth-of-type(4)", { opacity: 0, y: 15 }, "-=0.5")
   .to("#tablet-text span:nth-of-type(4)", { opacity: 0, y: -15 }, "+=0.5")
   .from("#tablet-text span:nth-of-type(5)", { opacity: 0, y: 15 }, "-=0.5")
-  .to("#tablet-title", { opacity: 0, y: -20, ease: "power1" })
-  .to("#tablet-text", { opacity: 0, duration: 0.5 }, "<")
-  .to("#tablets-svg", {
-    attr: { viewBox: "0 0 792.82 350" },
+  .to("#tablet-title", { opacity: 0, y: -20, ease: "power1" }, "+=0.5")
+  .to("#tablet-text", { opacity: 0, y: -10, ease: "power1" }, "<")
+  // .to("#tablets-svg", {
+  //   attr: { viewBox: "0 0 792.82 350" },
+  // })
+  .to("#tablet-img", { scale: 0.9, transformOrigin: "50% 50%" }, "<")
+  .from(".panel.two", { yPercent: 100, duration: 1.5 }, "-=0.5")
+  .from("#step1", {
+    y: 50,
+    opacity: 0,
+  })
+  .from(
+    "#tip1",
+    {
+      opacity: 0,
+    },
+    "+=0.5"
+  )
+  .to(
+    "#step1, #tip1",
+    {
+      y: -20,
+      opacity: 0,
+    },
+    "+=0.5"
+  )
+  .from("#step2", {
+    y: 50,
+    opacity: 0,
   })
   .to(
-    "#tablet-img",
-    { scale: 0.5, yPercent: 100, transformOrigin: "50% 50%" },
+    "#wave-dark-mask .wave-rotate",
+    {
+      rotate: 360,
+      transformOrigin: "50% 50%",
+      duration: 2,
+      ease: "linear",
+      repeat: 0.5,
+    },
     "<"
-  );
-//   .from(".panel.two", { yPercent: 100 }, "-=0.5")
-//   .from("#step1", {
-//     y: 50,
-//     opacity: 0,
-//   })
-//   .from("#tip1", {
-//     opacity: 0,
-//   })
-//   .to("#step1, #tip1", {
-//     y: -20,
-//     opacity: 0,
-//   })
-//   .from("#step2", {
-//     y: 50,
-//     opacity: 0,
-//   })
-//   .to(
-//     "#wave-dark-mask .wave-rotate",
-//     {
-//       rotate: 360,
-//       transformOrigin: "50% 50%",
-//       duration: 2,
-//       ease: "linear",
-//       repeat: 0.5,
-//     },
-//     "<"
-//   )
-//   .from(
-//     "#wave-light-mask .wave-rotate",
-//     {
-//       rotate: 360,
-//       transformOrigin: "50% 50%",
-//       duration: 2,
-//       ease: "linear",
-//       repeat: 0.5,
-//     },
-//     "<"
-//   )
-//   .from("#waves", { yPercent: 100, duration: 3 }, "<")
-//   .from("#tip2", {
-//     opacity: 0,
-//   })
-//   .from("#waves", { opacity: 0.7 }, "<")
-//   .to("#tablet-img", { opacity: 0 }, "<")
-//   .to("#step2, #tip2", {
-//     y: -20,
-//     opacity: 0,
-//   })
-//   .from("#step3", {
-//     y: 50,
-//     opacity: 0,
-//   })
-//   .add("cap")
-//   .from("#bottle-cap", { yPercent: -100, opacity: 0 }, "<")
-//   .from("#cap-mask-rect", { y: -500 }, "<")
-//   .to("#bottle-cap", { x: -8 }, "<")
-//   .fromTo("#bottle-cap", { x: -8 }, { x: 0, duration: 0.5 }, "-=0.5")
+  )
+  .from(
+    "#wave-light-mask .wave-rotate",
+    {
+      rotate: 360,
+      transformOrigin: "50% 50%",
+      duration: 2,
+      ease: "linear",
+      repeat: 0.5,
+    },
+    "<"
+  )
+  .from("#waves", { yPercent: 100, duration: 3 }, "<")
+  .to("#wave-light-mask .wave-rotate, #wave-dark-mask .wave-rotate", {
+    y: -30,
+  })
+  .from("#tip2", {
+    opacity: 0,
+  })
+  .from("#waves", { opacity: 0.7 }, "<")
+  .to("#tablet-img", { opacity: 0 }, "<")
+  .to(
+    "#step2, #tip2",
+    {
+      y: -20,
+      opacity: 0,
+    },
+    "+=0.5"
+  )
+  .from("#step3", {
+    y: 50,
+    opacity: 0,
+  })
+  .add("cap")
+  .from("#bottle-cap", { yPercent: -100, opacity: 0 }, "<")
+  .from("#cap-mask-rect", { y: -500 }, "<")
+  .to("#bottle-cap", { x: -8 }, "<")
+  .fromTo("#bottle-cap", { x: -8 }, { x: 0, duration: 0.5 }, "-=0.5")
 
-//   .from("#tip3", {
-//     opacity: 0,
-//   })
-//   .to("#step3, #tip3", {
-//     y: -20,
-//     opacity: 0,
-//   })
-//   .from("#bottle-blue", { opacity: 0 })
-//   .to("#bottle-cap, #bottle, #waves", { opacity: 0 }, "<")
-//   .to("#bottle-svg", {
-//     attr: { viewBox: "-105 -200 426.28 764.37" },
-//   })
-//   .from("#bottle-green, #bottle-yellow", { opacity: 0, duration: 0.1 }, "<")
-//   .to("#bottle-green", { xPercent: -30 }, "<")
-//   .to("#bottle-yellow", { xPercent: 30 }, "<")
-//   .from(".panel.two a", { opacity: 0, duration: 0.5 }, "-=0.5");
+  .from(
+    "#tip3",
+    {
+      opacity: 0,
+    },
+    "+=0.5"
+  )
+  .to(
+    "#step3, #tip3",
+    {
+      y: -20,
+      opacity: 0,
+    },
+    "+=0.5"
+  )
+  .from("#bottle-blue", { opacity: 0 })
+  .to("#bottle-cap, #bottle, #waves", { opacity: 0 }, "<")
+  .to("#bottle-svg", {
+    attr: { viewBox: "-105 -200 426.28 764.37" },
+  })
+  .from("#bottle-green, #bottle-yellow", { opacity: 0, duration: 0.1 }, "<")
+  .to(
+    "#bottle-green",
+    { xPercent: -33, yPercent: -5, scale: 0.9, transformOrigin: "50% 50%" },
+    "<"
+  )
+  .to(
+    "#bottle-yellow",
+    { xPercent: 34, yPercent: -5, scale: 0.88, transformOrigin: "50% 50%" },
+    "<"
+  )
+  .from(".panel.two a", { opacity: 0, duration: 0.5 }, "-=0.5");
